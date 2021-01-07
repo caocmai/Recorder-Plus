8edc116ee1a49e3badc8ec0c138bec34e22d6582
Cao Mai <caomai@rocketmail.com>
2021-01-06 20:19:10 -0800

    Frist upload to gitHub

---
+++ RecorderPlusTests/RecorderPlusTests.swift
@@ -0,0 +1,33 @@
+//
+//  RecorderPlusTests.swift
+//  RecorderPlusTests
+//
+//  Created by Cao Mai on 1/6/21.
+//
+
+import XCTest
+@testable import RecorderPlus
+
+class RecorderPlusTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
