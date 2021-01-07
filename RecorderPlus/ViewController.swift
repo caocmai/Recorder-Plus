09d5414c8d900b2ed24743e60e296cf1c1439e92
Cao Mai <caomai@rocketmail.com>
2021-01-06 21:53:04 -0800

    Add TableView and CollectionView

--- RecorderPlus/ViewController.swift
+++ RecorderPlus/ViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .blue
+        self.view.backgroundColor = .white
+        self.title = "HomeView"
+
+
     }
-
-
 }
-
---
+++ RecorderPlus/ViewController2.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController2.swift
+//  RecorderPlus
+//
+//  Created by Cao Mai on 1/6/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let test = ["a", "b", "c", "d"]
+    let table = UITableView()
+    
+    let simpleConfig = UICollectionView.CellRegistration<MyCollectionViewCell, String> { (cell, indexPath, model) in
+      cell.label.text = model
+
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.title = "HomeView2"
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
+        
+        let myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+
+        
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+//        myCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CVcell")
+        myCollectionView.backgroundColor = .white
+        self.view.addSubview(myCollectionView)
+                
+        if let flowLayout = myCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .horizontal
+        }
+        
+
+    }
+    
+    func setupTable(){
+        view.addSubview(table)
+        table.delegate = self
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.frame = view.bounds
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = test[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+
+
+}
+
+
+extension ViewController2 {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 19
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CVcell", for: indexPath)
+//        myCell.backgroundColor = UIColor.blue
+//        return myCell
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           print("User tapped on item \(indexPath.row)")
+        }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let model = "Cell \(indexPath.row)"
+
+      return collectionView.dequeueConfiguredReusableCell(using: simpleConfig,
+                                                          for: indexPath,
+                                                          item: model)
+    }
+}
+
+
+
+class MyCollectionViewCell: UICollectionViewCell {
+  let label = UILabel()
+
+  required init?(coder: NSCoder) {
+    fatalError("nope!")
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    contentView.addSubview(label)
+    contentView.backgroundColor = .blue
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+    label.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+    label.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant:  -8).isActive = true
+    label.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+  }
+}
